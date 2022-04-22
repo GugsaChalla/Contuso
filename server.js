3bbeb0f484931c0d514579a1ad65f4a22fa7c814
Gugsa Challa <gchalla8@gmail.com>
2022-04-21 20:06:01 -0400

    server setup

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+if(process.env_NODE_ENV!== 'production') {
+    require('dotenv').load();
+}
 
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
+
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const stripe = require('stripe')(stripeSecretKey);
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.static('public'));
+
+app.listen(3000);
